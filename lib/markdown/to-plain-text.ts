00568c6aecd32ea0aa73517a16baf619fdb51d1a
mshrynzw <iq87io25@gmail.com>
2025-10-26 09:59:08 +0900

    fix(all) edit: to-plain-text.ts

--- lib/markdown/to-plain-text.ts
+++ lib/markdown/to-plain-text.ts
@@ -24,12 +24,12 @@ export function markdownToPlainText(markdown: string): string {
 
   // --- 見出し ---
   text = text
-    .replace(/^###### (.*)$/gm, '───── ● $1')
-    .replace(/^##### (.*)$/gm, '──── ● $1')
-    .replace(/^#### (.*)$/gm, '─── ● $1')
-    .replace(/^### (.*)$/gm, '── ● $1')
-    .replace(/^## (.*)$/gm, '■ $1')
-    .replace(/^# (.*)$/gm, '──────────────\n ■ $1\n──────────────');
+    .replace(/^###### (.*)$/gm, '\n───── ● $1')
+    .replace(/^##### (.*)$/gm, '\n──── ● $1')
+    .replace(/^#### (.*)$/gm, '\n─── ● $1')
+    .replace(/^### (.*)$/gm, '\n── ● $1')
+    .replace(/^## (.*)$/gm, '\n■ $1\n──────────────')
+    .replace(/^# (.*)$/gm, '\n──────────────\n ■ $1\n──────────────');
 
   // --- 太字・斜体・取り消し線 ---
   text = text
